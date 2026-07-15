6c629cceb04bb440f6ad076bbee0708dbaf09c3e
kasra <kasranikshabani@yahoo.com>
2026-07-15 22:56:03 +0330

    deploy

--- football_tickets/settings.py
+++ football_tickets/settings.py
@@ -62,7 +62,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'sepahan_ticket'),
         'USER': os.getenv('DB_USER', 'sepahan_user'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'kasra123'),
         'HOST': os.getenv('DB_HOST', '127.0.0.1'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
